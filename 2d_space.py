2644484d7e9f5f3c5fcd314ade178d095fefebe0
Mykhailo Poliakov <stone4onelove@gmail.com>
2026-02-01 16:10:54 +0100

    looks cool

--- 2d_space.py
+++ 2d_space.py
@@ -11,56 +11,81 @@ background_color = (0, 5, 0)
 screen.fill(background_color)
 
 points = { #(x,y,z)
-    '1' : [-100.0,  100.0,  100.0],
-    '2' : [ 100.0,  100.0,  100,0],
-    '3' : [-100.0, -100.0, -100.0],
-    '4' : [ 100.0, -100.0, -100.0],
-    '5' : [-100.0,  100.0, -100.0],
-    '6' : [ 100.0,  100.0, -100,0],
-    '7' : [-100.0, -100.0,  100.0],
-    '8' : [ 100.0, -100.0,  100.0],
+    'square' : {
+        '1' : [-100.0,  100.0,  100.0],
+        '2' : [ 100.0,  100.0,  100,0],
+        '3' : [-100.0, -100.0, -100.0],
+        '4' : [ 100.0, -100.0, -100.0],
+        '5' : [-100.0,  100.0, -100.0],
+        '6' : [ 100.0,  100.0, -100,0],
+        '7' : [-100.0, -100.0,  100.0],
+        '8' : [ 100.0, -100.0,  100.0],
+    },
+    'plane' : {
+        '1' : [  200.0, -100.0, 200.0],
+        '2' : [ -200.0, -100.0, 200.0],
+        '3' : [  200.0, -100.0,-200.0],
+        '4' : [ -200.0, -100.0,-200.0],
+    }
 }
 
-def print_point(_dict,_point,local_center,):
-    mult = ((_dict[_point][2] + 100) / 200) + 2
-    if 55 <= (_dict[_point][2] + 100) + 55 <= 255:
-        _color = (_dict[_point][2] + 100) + 55
-    elif 55 > (_dict[_point][2] + 100) + 55:
+for i in range(-20, 21):
+    points['plane']['x1' + str(i)] = [ i * 15,-100, 300]
+    points['plane']['x2' + str(i)] = [ i * 15,-100,-300]
+    points['plane']['z1' + str(i)] = [ 300,-100, i * 15]
+    points['plane']['z2' + str(i)] = [-300,-100, i * 15]
+    points['plane']['x3' + str(i)] = [i * 15, -100, 200]
+    points['plane']['x4' + str(i)] = [i * 15, -100, -200]
+    points['plane']['z3' + str(i)] = [200, -100, i * 15]
+    points['plane']['z4' + str(i)] = [-200, -100, i * 15]
+    points['plane']['x5' + str(i)] = [i * 15, -100, 100]
+    points['plane']['x6' + str(i)] = [i * 15, -100, -100]
+    points['plane']['z5' + str(i)] = [100, -100, i * 15]
+    points['plane']['z6' + str(i)] = [-100, -100, i * 15]
+    points['plane']['x7' + str(i)] = [i * 15, -100, 0]
+    points['plane']['z7' + str(i)] = [0, -100, i * 15]
+
+for i in range(-20, 21):
+    points['square']['x1' + str(i)] = [ i * 5, 100, 100]
+    points['square']['x2' + str(i)] = [ i * 5,-100, 100]
+    points['square']['x3' + str(i)] = [ i * 5,-100,-100]
+    points['square']['x4' + str(i)] = [ i * 5, 100,-100]
+    points['square']['y1' + str(i)] = [ 100, i * 5, 100]
+    points['square']['y2' + str(i)] = [-100, i * 5, 100]
+    points['square']['y3' + str(i)] = [-100, i * 5,-100]
+    points['square']['y4' + str(i)] = [ 100, i * 5,-100]
+    points['square']['z1' + str(i)] = [ 100, 100, i * 5]
+    points['square']['z2' + str(i)] = [-100, 100, i * 5]
+    points['square']['z3' + str(i)] = [-100,-100, i * 5]
+    points['square']['z4' + str(i)] = [ 100,-100, i * 5]
+
+def print_point(obj_name,_point,local_center, choose_color,size=10):
+    mult = ((points[obj_name][_point][2] + 100) / 200) + 2
+    if 55 <= (points[obj_name][_point][2] + 100) + 55 <= 255:
+        _color = (points[obj_name][_point][2] + 100) + 55
+        _size = (points[obj_name][_point][2] + 100) / 20 + 5
+    elif 55 > (points[obj_name][_point][2] + 100) + 55:
         _color = 55
+        _size = 5
     else:
         _color = 255
-    pygame.draw.rect(screen, (0, _color, 0),
-                    (local_center[0] + _dict[_point][0] * mult, local_center[1] - _dict[_point][1] * mult, 10, 10))
+        _size = 15
+    pygame.draw.rect(screen, (0, _color, choose_color),
+                    (local_center[0] + points[obj_name][_point][0] * mult,
+                          local_center[1] - points[obj_name][_point][1] * mult, _size, _size))
 
-def print_polygon(_dict,_point,local_center, polygon):
-    mult = ((_dict[_point][2] + 100) / 200) + 2
-    if _dict[_point][0] > 0:
-        polygon.pol_dict['x+'][_point] = (local_center[0] + _dict[_point][0] * mult, local_center[1] - _dict[_point][1] * mult)
-    if _dict[_point][0] < 0:
-        polygon.pol_dict['x-'][_point] = (local_center[0] + _dict[_point][0] * mult, local_center[1] - _dict[_point][1] * mult)
-    if _dict[_point][0] > 0:
-        polygon.pol_dict['y+'][_point] = (local_center[0] + _dict[_point][0] * mult, local_center[1] - _dict[_point][1] * mult)
-    if _dict[_point][0] < 0:
-        polygon.pol_dict['y-'][_point] = (local_center[0] + _dict[_point][0] * mult, local_center[1] - _dict[_point][1] * mult)
-    if _dict[_point][0] > 0:
-        polygon.pol_dict['z+'][_point] = (local_center[0] + _dict[_point][0] * mult, local_center[1] - _dict[_point][1] * mult)
-    if _dict[_point][0] < 0:
-        polygon.pol_dict['z-'][_point] = (local_center[0] + _dict[_point][0] * mult, local_center[1] - _dict[_point][1] * mult)
+class CameraChanger:
+    def __init__(self):
+        self.point_dict = {}
+        for object_name in all_objects:
+            for dot_name in object_name.point_dict:
+                self.point_dict[f'{object_name}_{dot_name}'] = object_name.point_dict[dot_name]
 
 
 
 class CordChanger:
     def __init__(self, input_points):
         self.point_dict = input_points
-        self.pol_dict = {
-            'x+' : {},
-            'x-' : {},
-            'y+' : {},
-            'y-' : {},
-            'z+' : {},
-            'z-' : {},
-        }
-        self.dots = []
 
     def __get_angle(self,_point, index):
         radius = math.hypot(self.point_dict[_point][index[0]], self.point_dict[_point][index[1]])
@@ -92,7 +117,11 @@ class CordChanger:
             z_cord = round(radius * math.cos(math.radians(angle)), 2)
         self.point_dict[_point] = [x_cord, y_cord, z_cord]
 
-square = CordChanger(points)
+# objects
+square = CordChanger(points['square'])
+plane = CordChanger(points['plane'])
+all_objects = [square]
+
 running = True
 while running:
     """ BRAIN """
@@ -106,32 +135,25 @@ while running:
 
     keys = pygame.key.get_pressed()
     if keys[pygame.K_LEFT] and not keys[pygame.K_RIGHT]:
-        for point in points:
+        for point in points['square']:
             square.add_angle(point, (0,2),1)
     elif keys[pygame.K_RIGHT] and not keys[pygame.K_LEFT]:
-        for point in points:
+        for point in points['square']:
             square.add_angle(point,(0,2), -1)
     if keys[pygame.K_UP] and not keys[pygame.K_DOWN]:
-        for point in points:
+        for point in points['square']:
             square.add_angle(point, (1,2),-1)
     elif keys[pygame.K_DOWN] and not keys[pygame.K_UP]:
-        for point in points:
+        for point in points['square']:
             square.add_angle(point,(1,2), 1)
 
     """ OUTPUT """
     screen.fill(background_color)
     pygame.draw.rect(screen, (255, 255, 0),(center[0], center[1], 5, 5))
-    for point in points:
-        print_point(points, point, center)
-        #print_polygon(points,point,center,square)
-
-    '''
-    for keys in square.pol_dict:
-        square.dots = []
-        for keys2 in square.pol_dict[keys]:
-           square.dots.append(square.pol_dict[keys][keys2])
-        pygame.draw.polygon(screen, (0, 255, 0), square.dots)
-    '''
+    #for point in points['plane']:
+        #print_point('plane', point, center, 255,20)
+    for point in points['square']:
+        print_point('square', point, center, 0)
 
     pygame.display.flip()
     clock.tick(60)
