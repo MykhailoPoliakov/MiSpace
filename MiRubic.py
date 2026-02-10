bbd7148b6c41549b148f64d5086b689cf6617f73
Mykhailo Poliakov <stone4onelove@gmail.com>
2026-02-10 16:42:41 +0100

    started working on rubik

---
+++ MiRubic.py
@@ -0,0 +1,337 @@
+import pygame
+import math
+
+center = (1190, 540)
+mouse_cords = center
+var = {'active' : True, 'analytic' : True}
+all_objects = []
+all_cameras = []
+object_order = []
+points = {}
+colors = {
+    'red'   : (255,   0,   0),
+    'green' : (  0, 255,   0),
+    'blue'  : (  0,   0, 255),
+    'yellow': (255, 255,   0),
+    'orange': (255, 165,   0),
+    'white' : (255, 255, 255),
+    'gray'  : (127, 127, 127),
+}
+
+
+def angle_calc(_radius, _cord_0, _cord_1):
+    _angle = math.degrees(math.acos(_cord_1 / _radius)) if _radius != 0 else 0
+    return 360 - _angle if _cord_0 < 0 else _angle
+
+def render_object(_object, choose_color):
+    for _point in points[_object.name]:
+        # z cord (depth calculation)
+        mult = (((camera.output[_object.name][_point][2] + 125) / 375) + 2)
+        # x and y cord output
+        pygame.draw.rect(screen, (0, 0, choose_color),
+                        (center[0] + (camera.output[_object.name][_point][0]) * mult,
+                        center[1] - (camera.output[_object.name][_point][1]) * mult, 10, 10))
+
+def calculate_polygon(_object):
+
+    def calculate_color(_depth, _colors):
+        f_colors = []
+        if _depth > 100:
+            return _colors
+        elif _depth < -100:
+            return (_colors[1] / 1.5, _colors[1] / 1.5, _colors[2] / 1.5),
+        for _color in _colors:
+            f_colors.append(int(_color / (1 + ((_depth - 100) / -40))))
+        return f_colors
+
+    def sort(_order, condition, content):
+        if _order:
+            for _num in range(len(_order)):
+                if condition > _order[_num][1]:
+                    _order.insert(_num, content)
+                    return
+        _order.append(content)
+        return
+
+    polygons = {}
+    order = []
+    polygon_depth = 0
+    for polygon, color in _object.polygons:
+        polygons[polygon] = {'render_points' : [], 'depth_ev' : 0}
+        for _point in polygon:
+            # z cord (depth calculation)
+            mult = round((((camera.output[_object.name][_point][2] + 125) / 375) + 2),2)
+            polygons[polygon]['render_points'].append((round(center[0] + (camera.output[_object.name][_point][0]) * mult,2),
+                                    round(center[1] - (camera.output[_object.name][_point][1]) * mult,2)))
+            depth = int(camera.output[_object.name][_point][2])
+            polygons[polygon]['depth_ev'] += depth
+            polygon_depth += depth
+        # color of the polygon calculation
+        final_color = calculate_color(polygons[polygon]['depth_ev'] / 4, color)
+        # in "order" sorting all polygons of an object
+        sort(order, polygons[polygon]['depth_ev'], (polygon, polygons[polygon]['depth_ev'],tuple(final_color)))
+    # in "object_order" sorting object itself
+    sort(object_order, polygon_depth , (_object, polygon_depth, order[::-1],polygons))
+
+def render_polygon(object_info):
+    for _polygon, _depth, _color in object_info[2]:
+        pygame.draw.polygon(screen, _color, object_info[3][_polygon]['render_points'])
+        pygame.draw.polygon(screen, (0, 0, 0) , object_info[3][_polygon]['render_points'],4)
+
+class CameraChanger:
+    def __init__(self,name):
+        all_cameras.append(self)
+        self.name = name
+        self.rotation = [0,0]
+        self.output = {}
+        self.size = 1
+        for obj in points:
+            self.output[obj] = {}
+            for dott in points[obj]:
+                self.output[obj][dott] = [0, 0, 0]
+
+    def rotate(self,index, add_ang):
+        self.rotation[index] += add_ang
+        var['active'] = True
+
+    def resize(self,add_size):
+        self.size *= 1 + add_size / 100
+        var['active'] = True
+
+    def render(self):
+        for _obj in all_objects:
+            for _point in points[_obj.name]:
+                # camera y rotation offset
+                index = (0,2,1)
+                radius = math.hypot(points[_obj.name][_point][index[0]] + _obj.pos[index[0]],
+                                    points[_obj.name][_point][index[1]] + _obj.pos[index[1]])
+                angle = angle_calc(radius, points[_obj.name][_point][index[0]] + _obj.pos[index[0]],
+                                   points[_obj.name][_point][index[1]] + _obj.pos[index[1]]) - self.rotation[index[2]]
+                cord_x = points[_obj.name][_point][index[2]] + _obj.pos[index[2]]
+                cord_y = round(radius * math.sin(math.radians(angle)), 2)
+                cord_z = round(radius * math.cos(math.radians(angle)), 2)
+                # camera x rotation offset
+                index = (1, 2, 0)
+                radius = math.hypot(cord_x, cord_z)
+                angle = angle_calc(radius, cord_x, cord_z) - self.rotation[index[2]]
+                # final camera cord output
+                self.output[_obj.name][_point][index[0]] = round(radius * math.sin(math.radians(angle)) * self.size, 2)
+                self.output[_obj.name][_point][index[2]] = round(cord_y * self.size,2)
+                self.output[_obj.name][_point][index[1]] = round(radius * math.cos(math.radians(angle)) * self.size, 2)
+
+class ObjectChanger:
+    def __init__(self, obj_name,polygons=None,_points=None):
+        all_objects.append(self)
+        if _points:
+            points[obj_name] = _points
+        self.point_dict = points[obj_name]
+        self.name = obj_name
+        self.pos = [0,0,0]
+        self.rotation = [0, 0, 0]
+        self.size = 1
+        self.polygons = polygons
+
+    def rotate(self,index, add_ang):
+        for _point in points[active_object.name]:
+            radius = math.hypot(self.point_dict[_point][index[0]], self.point_dict[_point][index[1]])
+            angle = angle_calc(radius, self.point_dict[_point][index[0]], self.point_dict[_point][index[1]]) - add_ang
+            self.point_dict[_point][index[0]] = round(radius * math.sin(math.radians(angle)), 2)
+            self.point_dict[_point][index[1]] = round(radius * math.cos(math.radians(angle)), 2)
+        self.rotation[1] += 1
+        var['active'] = True
+
+    def move(self, index, add_move):
+        self.pos[index] += add_move
+        var['active'] = True
+
+    def resize(self,add_size):
+        for _point in self.point_dict:
+            for index in [0,1,2]:
+                self.point_dict[_point][index] *= round(1 + add_size / 100,2)
+        self.size *= 1 + add_size / 100
+        var['active'] = True
+
+def create_cube(_obj_name,points_offset, color_input):
+    cube_color = ('red','yellow','green','blue','white','orange')
+    color_output = ['gray','gray','gray','gray','gray','gray']
+    for _num in color_input:
+        color_output[_num -1] = cube_color[_num-1]
+
+    colored = ((('1', '2', '3', '4'), colors[color_output[0]]), (('5', '6', '7', '8'), colors[color_output[1]]),
+               (('3', '4', '8', '7'), colors[color_output[2]]), (('1', '2', '6', '5'), colors[color_output[3]]),
+               (('1', '4', '8', '5'), colors[color_output[4]]), (('2', '3', '7', '6'), colors[color_output[5]]))
+    cube_points = {
+        '1': [ 33.0, 33.0, 33.0], '2': [ 33.0,-33.0, 33.0],
+        '3': [-33.0,-33.0, 33.0], '4': [-33.0, 33.0, 33.0],
+        '5': [ 33.0, 33.0,-33.0], '6': [ 33.0,-33.0,-33.0],
+        '7': [-33.0,-33.0,-33.0], '8': [-33.0, 33.0,-33.0]}
+    for point in cube_points:
+        cube_points[point][0] += points_offset[0]
+        cube_points[point][1] += points_offset[1]
+        cube_points[point][2] += points_offset[2]
+    return [_obj_name,colored,cube_points]
+
+
+"""Objects"""
+# corners
+corner_lfu = ObjectChanger(*create_cube('corner_lfu',(-66, 66, 66), (1,3,5)))
+corner_lfd = ObjectChanger(*create_cube('corner_lfd',(-66,-66, 66), (1,3,6)))
+corner_rfu = ObjectChanger(*create_cube('corner_rfu',( 66, 66, 66), (1,4,5)))
+corner_rfd = ObjectChanger(*create_cube('corner_rfd',( 66,-66, 66), (1,4,6)))
+corner_lbu = ObjectChanger(*create_cube('corner_lbu',(-66, 66,-66), (2,3,5)))
+corner_lbd = ObjectChanger(*create_cube('corner_lbd',(-66,-66,-66), (2,3,6)))
+corner_rbu = ObjectChanger(*create_cube('corner_rbu',( 66, 66,-66), (2,4,5)))
+corner_rbd = ObjectChanger(*create_cube('corner_rbd',( 66,-66,-66), (2,4,6)))
+# cuts
+cut_lu = ObjectChanger(*create_cube('cut_lu',(-66, 66, 0), (3,5)))
+
+rubik = {
+    'lfu' : corner_lfu, 'lfd' : corner_lfd, 'rfu' : corner_rfu,
+
+}
+# cameras
+camera1 = CameraChanger('Camera 1')
+camera2 = CameraChanger('Camera 2')
+
+active_object = all_objects[0]
+camera = all_cameras[0]
+
+# pygame initialization
+pygame.init()
+screen = pygame.display.set_mode((1980, 1080), pygame.FULLSCREEN | pygame.SCALED)
+clock = pygame.time.Clock()
+background_color = (0, 5, 0)
+screen.fill(background_color)
+
+# main cycle
+running = True
+while running:
+    """ BRAIN """
+    # objects output calculation
+    if var['active']:
+        var['active'] = False
+        camera.render()
+        object_order = []
+        for _object in all_objects:
+            if _object.polygons:
+                calculate_polygon(_object)
+
+    """ INPUT """
+    # rotation,movement and sizing
+    keys = pygame.key.get_pressed()
+    mouse_keys = pygame.mouse.get_pressed()
+
+    for event in pygame.event.get():
+        # ways to exit
+        if event.type == pygame.QUIT:
+            running = False
+        elif event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
+            running = False
+
+        # camera sizing with mouse
+        elif event.type == pygame.MOUSEWHEEL:
+            if (camera.size < 2 or event.y < 0) and (camera.size > 0.3 or event.y > 0):
+                camera.resize(event.y * 5)
+        # camera rotation with mouse and cursor control
+        elif event.type == pygame.MOUSEBUTTONDOWN and event.button == 3:
+            pygame.mouse.set_visible(False) ; pygame.event.set_grab(True)
+            mouse_cords = pygame.mouse.get_pos()
+        elif event.type == pygame.MOUSEBUTTONUP and event.button == 3:
+            pygame.mouse.set_visible(True) ; pygame.event.set_grab(False)
+            pygame.mouse.set_pos(mouse_cords)
+        elif event.type == pygame.MOUSEMOTION and mouse_keys[2]:
+            dx, dy = event.rel
+            if (camera.rotation[0] < 85 or dy < 0) and (camera.rotation[0] > -85 or dy > 0):
+                camera.rotate(0, dy / 20)
+            camera.rotate(1, -dx / 20)
+
+        # analytic mode
+        elif event.type == pygame.KEYDOWN and event.key == pygame.K_F3:
+            var['analytic'] = True if var['analytic'] == False else False
+            center = (1190, 540) if center == (990, 540) else (990, 540)
+            var['active'] = True
+
+        #object change
+        elif event.type == pygame.KEYDOWN and event.key == pygame.K_o:
+            _index = all_objects.index(active_object)
+            active_object = all_objects[_index + 1] if _index < len(all_objects) - 1 else all_objects[0]
+            var['active'] = True
+        # camera change
+        elif event.type == pygame.KEYDOWN and event.key == pygame.K_k:
+            _index = all_cameras.index(camera)
+            camera = all_cameras[_index + 1] if _index < len(all_cameras) - 1 else all_cameras[0]
+            var['active'] = True
+
+    # object movement
+    if keys[pygame.K_g]:
+        if keys[pygame.K_RIGHT] and not keys[pygame.K_LEFT]:
+            active_object.move(0,2)
+        elif keys[pygame.K_LEFT]  and not keys[pygame.K_RIGHT]:
+            active_object.move(0, -2)
+        if keys[pygame.K_RALT]    and not keys[pygame.K_DOWN]:
+            active_object.move(1,2)
+        elif keys[pygame.K_RCTRL]  and not keys[pygame.K_UP]:
+            active_object.move(1,-2)
+        if keys[pygame.K_DOWN]  and not keys[pygame.K_RCTRL]:
+            active_object.move(2, 2)
+        elif keys[pygame.K_UP] and not keys[pygame.K_RALT]:
+            active_object.move(2, -2)
+
+    # object resizing
+    elif keys[pygame.K_x]:
+        if keys[pygame.K_UP] and not keys[pygame.K_DOWN] and active_object.size < 3:
+            active_object.resize(1)
+        elif keys[pygame.K_DOWN] and not keys[pygame.K_UP] and active_object.size > 0.20:
+            active_object.resize(-1)
+
+    # object rotation
+    elif keys[pygame.K_r]:
+        if keys[pygame.K_LEFT] and not keys[pygame.K_RIGHT]:
+            active_object.rotate((0, 2), 1)
+        elif keys[pygame.K_RIGHT] and not keys[pygame.K_LEFT]:
+            active_object.rotate((0, 2),-1)
+        if keys[pygame.K_UP] and not keys[pygame.K_DOWN]:
+            active_object.rotate((1, 2), 1)
+        elif keys[pygame.K_DOWN] and not keys[pygame.K_UP]:
+            active_object.rotate((1, 2),-1)
+        if keys[pygame.K_RALT] and not keys[pygame.K_RCTRL]:
+            active_object.rotate((0, 1), 1)
+        elif keys[pygame.K_RCTRL] and not keys[pygame.K_RALT]:
+            active_object.rotate((0, 1),-1)
+
+    """ OUTPUT """
+    # background
+    screen.fill(background_color)
+    pygame.draw.rect(screen, (255, 255, 0),(center[0], center[1], 5, 5))
+
+    # objects output render
+    for _object in object_order[::-1]:
+        render_polygon(_object)
+
+    # in-game info output
+    if var['analytic']:
+        fps = int(clock.get_fps())
+        font = pygame.font.Font(None, 40)
+        messages = [
+
+        f'Fps : {fps}',
+        f'' ,
+        f'Objects : {[i.name for i in all_objects]}',
+        f'Object : {active_object.name}',
+        f'Rotation : x {active_object.rotation[0]:.0f}° y {active_object.rotation[1]:.0f}° z {active_object.rotation[2]:.0f}°',
+        f'Coordinates : x {active_object.pos[0]:.2f} y {active_object.pos[1]:.2f} z {-active_object.pos[2]:.2f}',
+        f'Size : {active_object.size:.2f}',
+        f'',
+        f'Cameras : {[i.name for i in all_cameras]}',
+        f'Camera : {camera.name}',
+        f'Rotation : x {camera.rotation[0]:.0f}° y {camera.rotation[1]:.0f}°',
+        f'Size : {camera.size:.2f}',
+        f'',
+        f'State : {'active' if var['active'] else 'passive'}']
+        for num, message in enumerate(messages):
+            text = font.render(message, True, (255, 255, 255))
+            screen.blit(text, (15, 5 + num * 35))
+
+    pygame.display.flip()
+    clock.tick(60)
+pygame.quit()
