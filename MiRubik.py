750e0436b4548ec942796fa10d48676b0378d311
Mykhailo Poliakov <stone4onelove@gmail.com>
2026-02-13 06:35:09 +0100

    mode 'game' fully done

--- MiRubik.py
+++ MiRubik.py
@@ -2,9 +2,9 @@ import pygame
 import math
 import random
 
-center = (1190, 540)
-var = {'active' : True, 'analytic' : True, 'animation' : [[],0,''],
-       'dir_check' : ['',False], 'out_text' : '','shuffle' : False} # globals
+center = (990, 540)
+var = {'active' : True, 'analytic' : False, 'animation' : [[],0,''],
+       'dir_check' : '', 'out_text' : '','shuffle' : False, 'mode' : 'menu'} # globals
 all_objects = []
 all_cameras = []
 points = {}
@@ -16,48 +16,82 @@ colors = {
     'orange': (255, 165,   0),
     'white' : (255, 255, 255),
     'gray'  : ( 50,  50,  50),}
+cord_to_index = {'x' : (1,2), 'y' : (0,2), 'z' : (0,1)}
+blocks_x = ['112', '122', '132', '232', '332', '322', '312', '212', '222']
+blocks_y = ['121', '122', '123', '223', '323', '322', '321', '221', '222']
+blocks_z = ['211', '221', '231', '232', '233', '223', '213', '212', '222']
 
-def rubik_rotation(rotation_cord):
-    # rotation properties
-    if   rotation_cord[0] == 'x':
-        _blocks = blocks_x ; index = (1,2)
-    elif rotation_cord[0] == 'y':
-        _blocks = blocks_y ; index = (0,2)
-    elif rotation_cord[0] == 'z':
-        _blocks = blocks_z ; index = (0,1)
-    # rotation animation
-    _cof = -1 if rotation_cord[1] in ['u','r'] else 1
-    if var['animation'][0][1] == rotation_cord[1]:
-        if var['animation'][1] < 90:
-            if var['shuffle']:
-                _add_ang = 30
-            else:
-                _add_ang = 10 if 10 <= var['animation'][1] <= 70 else 2
-            for _obj_location in _blocks:
-                rubik[_obj_location].rotate(index, _add_ang * _cof)
-            var['animation'][1] += _add_ang
-        else:
-            # delete center point
-            _blocks.pop(4)
-            # blocks change after rotation animation
-            index_add = []
-            if rotation_cord[0] == 'x':
-                index_add = _blocks[2:] + _blocks[:2] if rotation_cord[1] == 'd' else _blocks[-2:] + _blocks[:-2]
-            elif rotation_cord[0] == 'z':
-                index_add = _blocks[2:] + _blocks[:2] if rotation_cord[1] == 'd' else _blocks[-2:] + _blocks[:-2]
-            elif rotation_cord[0] == 'y':
-                index_add = _blocks[2:] + _blocks[:2] if rotation_cord[1] == 'r' else _blocks[-2:] + _blocks[:-2]
-            # rewriting position
-            _place_list = []
-            for _place in _blocks:
-                _place_list.append(rubik[_place])
-            for _place in index_add:
-                rubik[_place] = _place_list.pop(0)
-            var['animation'] = [[],0,'']
 
 def angle_calc(_radius, _cord_0, _cord_1):
     _angle = math.degrees(math.acos(_cord_1 / _radius)) if _radius != 0 else 0
     return 360 - _angle if _cord_0 < 0 else _angle
+
+
+""" Rubik Rotation Functions """
+def rubik_rotation(rotation_cord,_blocks,_index,_cof):
+    # rotation animation
+    if var['animation'][1] < 90:
+        if var['shuffle']:
+            _add_ang = 30
+        else:
+            _add_ang = 10 if 10 <= var['animation'][1] <= 70 else 2
+        for _obj_location in _blocks:
+            rubik[_obj_location].rotate(_index, _add_ang * _cof)
+        var['animation'][1] += _add_ang
+    else:
+        # rewriting position
+        _blocks.pop(-1)
+        off_blocks = _blocks[2:] + _blocks[:2] if rotation_cord[1] in ['d', 'r'] else _blocks[-2:] + _blocks[:-2]
+        place_save = []
+        for pl in _blocks:  place_save.append(rubik[pl])
+        for pl in off_blocks:  rubik[pl] = place_save.pop(0)
+        var['animation'] = [[], 0, '']
+
+def rubik_calculation():
+    coef = ()
+    # side rotation
+    if var['animation'][0][0][-1] in ['u', 'd'] and var['animation'][0][-1] in ['l', 'r']:
+        direction = f'y{var['animation'][0][-1]}'
+        coef = (10, ('u', 'd'))
+        blocks_ = blocks_y
+    # front and back side vertical rotation and center
+    elif var['animation'][0][0][-2] in ['f', 'b']:
+        if var['animation'][0][0][-1] == 'm':
+            direction = f'{'x' if var['animation'][0][-1] in ['u', 'd'] else 'y'}{var['animation'][0][-1]}'
+            blocks_ = blocks_x if var['animation'][0][-1] in ['u', 'd'] else blocks_y
+        elif var['animation'][0][0][-1] in ['l', 'r'] and var['animation'][0][-1] in ['u', 'd']:
+            direction = f'x{var['animation'][0][-1]}'
+            coef = (1, ('l', 'r'))
+            blocks_ = blocks_x
+        else:
+            var['animation'] = [[], 0, '']
+            return
+    # left and right side vertical rotation and center
+    elif var['animation'][0][0][-2] in ['l', 'r']:
+        if var['animation'][0][0][-1] == 'm':
+            direction = f'{'z' if var['animation'][0][-1] in ['u', 'd'] else 'y'}{var['animation'][0][-1]}'
+            blocks_ = blocks_z if var['animation'][0][-1] in ['u', 'd'] else blocks_y
+        elif var['animation'][0][0][-1] in ['l', 'r'] and var['animation'][0][-1] in ['u', 'd']:
+            direction = f'z{var['animation'][0][-1]}'
+            coef = (100, ('l', 'r'))
+            blocks_ = blocks_z
+        else:
+            var['animation'] = [[], 0, '']
+            return
+    else:
+        var['animation'] = [[], 0, '']
+        return
+    # if action was recognized
+    new_blocks = blocks_.copy()
+    if coef:
+        mult = -1 if var['animation'][0][0][-1] in ['l', 'u'] else 1
+        for i in range(9):
+            new_blocks[i] = str(int(blocks_[i]) + coef[0] * mult)
+    var['animation'][2] = direction
+    i_index = cord_to_index[var['animation'][2][0]]
+    _cof = -1 if var['animation'][2][1] in ['u', 'r'] else 1
+    var['animation'].append([new_blocks, i_index, _cof])
+
 
 """ Objects Creation Functions """
 def create_cube(_obj_name,points_offset, color_input):
@@ -99,6 +133,7 @@ def create_button(_obj_name,points_offset, direction):
         _points[_point][index[1]] += points_offset[1] * cof
         _points[_point][index[2]] += 100 * cof
     return [_obj_name,colored,_points]
+
 
 """ Camera Class """
 class CameraChanger:
@@ -194,6 +229,7 @@ class CameraChanger:
                     pygame.draw.polygon(screen, _color, obj_info[3][_polygon]['render_points'])
                     pygame.draw.polygon(screen, (25, 25, 25), obj_info[3][_polygon]['render_points'], 3)
 
+
 """ Object Class """
 class ObjectChanger:
     def __init__(self, obj_name,polygons,_points):
@@ -207,7 +243,7 @@ class ObjectChanger:
         self.polygons = polygons
 
     def rotate(self,index, _add_ang):
-        for _point in points[active_object.name]:
+        for _point in points[self.name]:
             radius = math.hypot(self.point_dict[_point][index[0]], self.point_dict[_point][index[1]])
             angle = angle_calc(radius, self.point_dict[_point][index[0]], self.point_dict[_point][index[1]]) - _add_ang
             self.point_dict[_point][index[0]] = round(radius * math.sin(math.radians(angle)), 2)
@@ -225,6 +261,7 @@ class ObjectChanger:
                 self.point_dict[_point][index] *= round(1 + add_size / 100,2)
         self.size *= 1 + add_size / 100
         var['active'] = True
+
 
 """Objects"""
 # corners
@@ -273,11 +310,6 @@ for num, side in enumerate(['f','r','b','l']):
     buttons[f'{side}{let[2]}'] = ObjectChanger(*create_button(f'button_{side}{let[2]}',(  0,-66),side))
     buttons[f'{side}{let[3]}'] = ObjectChanger(*create_button(f'button_{side}{let[3]}',( 66,  0),side))
 
-# cameras
-camera1 = CameraChanger('Camera 1',(20,45))
-camera2 = CameraChanger('Camera 2',(20,45))
-
-# default settings
 rubik = {# first layer (front)
         '111' : corner_lfu, '112' : cut_fu, '113' : corner_rfu,
         '121' : cut_lf    , '122' : side_f, '123' : cut_rf    ,
@@ -291,17 +323,19 @@ rubik = {# first layer (front)
         '321' : cut_lb    , '322' : side_b, '323' : cut_rb    ,
         '331' : corner_lbd, '332' : cut_bd, '333' : corner_rbd,}
 
-active_object = all_objects[0]
+# cameras
+camera1 = CameraChanger('Camera 1',(20,45))
+camera2 = CameraChanger('Camera 2',(20,45))
 camera = all_cameras[0]
 
-# pygame initialization
+""" Main Cycle """
 pygame.init()
 screen = pygame.display.set_mode((1920, 1080), pygame.FULLSCREEN | pygame.SCALED)
 clock = pygame.time.Clock()
 background_color = (0, 5, 0)
 screen.fill(background_color)
 
-# main cycle
+var['mode'] = 'game'
 running = True
 while running:
     """ BRAIN """
@@ -322,50 +356,12 @@ while running:
 
     # calculating blocks to move and the direction
     if var['animation'][0] and not var['animation'][2]:
-        var['dir_check'] = ['', False]
-        blocks_x = ['112', '122', '132', '232', '222', '332', '322', '312', '212']
-        blocks_y = ['121', '122', '123', '223', '222', '323', '322', '321', '221']
-        blocks_z = ['211', '221', '231', '232', '222', '233', '223', '213', '212']
-
-        directions = [] ; coef = ()
-        # side rotation
-        if var['animation'][0][0][-1] in ['u', 'd'] and var['animation'][0][-1] in ['l', 'r']:
-            directions = ['yl', 'yr']
-            coef = (10,('u','d')); blocks_ = blocks_y
-        # front and back side vertical rotation and center
-        elif var['animation'][0][0][-2] in ['f','b']:
-            if var['animation'][0][0][-1] == 'm':
-                directions = ['xd', 'xu', 'yl', 'yr']
-                blocks_ = blocks_x if var['animation'][0][-1] in ['u','d'] else blocks_y
-            elif var['animation'][0][0][-1] in ['l','r'] and var['animation'][0][-1] in ['u','d']:
-                directions = ['xd', 'xu']
-                coef = (1,('l','r')) ; blocks_ = blocks_x
-        # left and right side vertical rotation and center
-        elif var['animation'][0][0][-2] in ['l', 'r']:
-            if var['animation'][0][0][-1] == 'm':
-                directions = ['zd', 'zu', 'yl', 'yr']
-                blocks_ = blocks_z if var['animation'][0][-1] in ['u','d'] else blocks_y
-            elif var['animation'][0][0][-1] in ['l','r'] and var['animation'][0][-1] in ['u','d']:
-                directions = ['zd', 'zu']
-                coef = (100,('l','r')) ; blocks_ = blocks_z
-
-        if directions:
-            if coef:
-                for num1,c1 in zip((-1,1),coef[1]):
-                    if var['animation'][0][0][-1] == c1:
-                        for num in range(9):
-                            blocks_[num] = str(int(blocks_[num]) + coef[0] * num1)
-                        break
-            for letter in directions:
-                if var['animation'][0][1] == letter[1]:
-                    var['animation'][2] = letter
-                    break
-        else:
-            var['animation'] = [[],0,'']
+        var['dir_check'] = ''
+        rubik_calculation()
 
     # rotation
     if var['animation'][2]:
-        rubik_rotation(var['animation'][2])
+        rubik_rotation(var['animation'][2],*var['animation'][3])
 
 
     """ INPUT """
@@ -379,72 +375,75 @@ while running:
         elif event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
             running = False
 
-        # camera sizing with mouse
-        elif event.type == pygame.MOUSEWHEEL:
-            if (camera.size < 2 or event.y < 0) and (camera.size > 0.3 or event.y > 0):
-                camera.resize(event.y * 5)
-        # camera rotation with mouse and cursor control
-        elif event.type == pygame.MOUSEBUTTONDOWN and event.button == 3:
-            pygame.mouse.set_visible(False) ; pygame.event.set_grab(True)
-            mouse_cords = pygame.mouse.get_pos()
-        elif event.type == pygame.MOUSEBUTTONUP and event.button == 3:
-            pygame.mouse.set_visible(True) ; pygame.event.set_grab(False)
-            pygame.mouse.set_pos(mouse_cords)
-        elif event.type == pygame.MOUSEMOTION and mouse_keys[2]:
-            dx, dy = event.rel
-            if (camera.rotation[0] < 85 or dy < 0) and (camera.rotation[0] > -85 or dy > 0):
-                camera.rotate(0, dy / 20)
-            camera.rotate(1, -dx / 20)
 
-        # analytic mode
-        elif event.type == pygame.KEYDOWN and event.key == pygame.K_F3:
-            var['analytic'] = True if var['analytic'] == False else False
-            center = (1190, 540) if center == (990, 540) else (990, 540)
-            var['active'] = True
+        if var['mode'] == 'game':
+            # camera sizing with mouse
+            if event.type == pygame.MOUSEWHEEL:
+                if (camera.size < 2 or event.y < 0) and (camera.size > 0.3 or event.y > 0):
+                    camera.resize(event.y * 5)
+            # camera rotation with mouse and cursor control
+            elif event.type == pygame.MOUSEBUTTONDOWN and event.button == 3:
+                pygame.mouse.set_visible(False) ; pygame.event.set_grab(True)
+                mouse_cords = pygame.mouse.get_pos()
+            elif event.type == pygame.MOUSEBUTTONUP and event.button == 3:
+                pygame.mouse.set_visible(True) ; pygame.event.set_grab(False)
+                pygame.mouse.set_pos(mouse_cords)
+            elif event.type == pygame.MOUSEMOTION and mouse_keys[2]:
+                dx, dy = event.rel
+                if (camera.rotation[0] < 85 or dy < 0) and (camera.rotation[0] > -85 or dy > 0):
+                    camera.rotate(0, dy / 20)
+                camera.rotate(1, -dx / 20)
 
-        elif event.type == pygame.KEYDOWN and event.key == pygame.K_b:
-            var['shuffle'] = True if var['shuffle'] == False else False
+            # analytic mode
+            elif event.type == pygame.KEYDOWN and event.key == pygame.K_F3:
+                var['analytic'] = True if var['analytic'] == False else False
+                center = (1190, 540) if center == (990, 540) else (990, 540)
+                var['active'] = True
 
-        # camera change
-        elif event.type == pygame.KEYDOWN and event.key == pygame.K_k:
-            _index = all_cameras.index(camera)
-            camera = all_cameras[_index + 1] if _index < len(all_cameras) - 1 else all_cameras[0]
-            var['active'] = True
+            # shuffle mode activation
+            elif event.type == pygame.KEYDOWN and event.key == pygame.K_b:
+                var['shuffle'] = True if var['shuffle'] == False else False
 
-        # informating if any button was activated
-        elif not var['animation'][0]:
-            if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1 and not mouse_keys[2]:
-                new_list = []
-                for _object in camera.object_order:
-                    if _object[0].name[:6] == 'button' and _object[1] > 0:
-                        for point in list(_object[3][('1', '2', '3', '4')]['render_points']):
-                            new_list.append((point[0], point[1]))
-                        button_name = _object[0].name
-                        # mask creation
-                        surf = pygame.Surface((1920, 1080), pygame.SRCALPHA)
-                        pygame.draw.polygon(surf, (255, 255, 255), new_list)
-                        mask = pygame.mask.from_surface(surf)
-                        mx, my = pygame.mouse.get_pos()
-                        # check if click was in the polygon
-                        if mask.get_at((mx, my)):
-                            var['dir_check'] = [button_name,True]
-                            dx, dy = 0,0
-                            break
-            if var['dir_check'][1]:
-                if event.type == pygame.MOUSEMOTION and mouse_keys[0]:
-                    dx += event.rel[0] ; dy += event.rel[1]
-                    # inversion of mirrored side
-                    if   dx >  30:
-                        var['animation'] = [[button_name,'r'], 0,'']
-                    elif dy < -30:
-                        inv_let = 'd' if button_name[-2] in ['b', 'r'] and button_name[-1] not in ['d', 'u'] else 'u'
-                        var['animation'] = [[button_name, inv_let], 0,'']
-                    elif dx < -30:
-                        var['animation'] = [[button_name,'l'], 0,'']
-                    elif dy >  30:
-                        inv_let = 'u' if button_name[-2] in ['b', 'r'] and button_name[-1] not in ['d', 'u'] else 'd'
-                        var['animation'] = [[button_name, inv_let], 0,'']
-                var['out_text'] = var['animation'] # for visualization
+            # camera change
+            elif event.type == pygame.KEYDOWN and event.key == pygame.K_k:
+                _index = all_cameras.index(camera)
+                camera = all_cameras[_index + 1] if _index < len(all_cameras) - 1 else all_cameras[0]
+                var['active'] = True
+
+            # informating if any button was activated
+            elif not var['animation'][0]:
+                if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1 and not mouse_keys[2]:
+                    new_list = []
+                    for _object in camera.object_order:
+                        if _object[0].name[:6] == 'button' and _object[1] > 0:
+                            for point in list(_object[3][('1', '2', '3', '4')]['render_points']):
+                                new_list.append((point[0], point[1]))
+                            button_name = _object[0].name
+                            # mask creation
+                            surf = pygame.Surface((1920, 1080), pygame.SRCALPHA)
+                            pygame.draw.polygon(surf, (255, 255, 255), new_list)
+                            mask = pygame.mask.from_surface(surf)
+                            mx, my = pygame.mouse.get_pos()
+                            # check if click was in the polygon
+                            if mask.get_at((mx, my)):
+                                var['dir_check'] = button_name
+                                dx, dy = 0,0
+                                break
+                if var['dir_check']:
+                    if event.type == pygame.MOUSEMOTION and mouse_keys[0]:
+                        dx += event.rel[0] ; dy += event.rel[1]
+                        # inversion of mirrored side
+                        if   dx >  30:
+                            var['animation'] = [[button_name,'r'], 0,'']
+                        elif dy < -30:
+                            inv_let = 'd' if button_name[-2] in ['b', 'r'] and button_name[-1] not in ['d','u'] else 'u'
+                            var['animation'] = [[button_name, inv_let], 0,'']
+                        elif dx < -30:
+                            var['animation'] = [[button_name,'l'], 0,'']
+                        elif dy >  30:
+                            inv_let = 'u' if button_name[-2] in ['b', 'r'] and button_name[-1] not in ['d','u'] else 'd'
+                            var['animation'] = [[button_name, inv_let], 0,'']
+                    var['out_text'] = var['animation'] # for visualization
 
     """ OUTPUT """
     # background
@@ -461,14 +460,13 @@ while running:
         messages = [
 
         f'Fps : {fps}',
+        f'State : {'active' if var['active'] else 'passive'}',
+        f'last rubik rotation : {var['out_text']}'
         f'' ,
         f'Cameras : {[i.name for i in all_cameras]}',
         f'Camera : {camera.name}',
         f'Rotation : x {camera.rotation[0]:.0f}° y {camera.rotation[1]:.0f}°',
-        f'Size : {camera.size:.2f}',
-        f'',
-        f'State : {'active' if var['active'] else 'passive'}',
-        f'last rubik rotation : {var['out_text']}']
+        f'Size : {camera.size:.2f}',]
 
         for num, message in enumerate(messages):
             text = font.render(message, True, (255, 255, 255))
